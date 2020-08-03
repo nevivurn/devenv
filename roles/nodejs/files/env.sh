1a92c13a2d19046d410bcb336b1851f895498af7
Yongun Seong <nevivurn@riseup.net>
2020-08-03 22:19:39 +0900

    nodejs: separate node/npm path from root

--- roles/nodejs/files/env.sh
+++ roles/nodejs/files/env.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
 DEVENV_NODEJS_BIN="${DEVENV_ROOT}/nodejs/current"
+export NODE_PATH="${DEVENV_ROOT}/nodejs/nodepath/lib/node_modules"
 
 if [[ -n "${DEVENV_DEBUG}" ]]; then
 	echo "[devenv/nodejs] nodejs version: $("${DEVENV_NODEJS_BIN}/bin/node" --version)"
 fi
 
 if [[ -z "${DEVENV_NODEJS_SETUP}" ]]; then
-	export PATH="${DEVENV_NODEJS_BIN}/bin:${PATH}"
+	export PATH="${DEVENV_ROOT}/nodejs/nodepath/bin:${DEVENV_NODEJS_BIN}/bin:${PATH}"
 fi
 export DEVENV_NODEJS_SETUP=1
