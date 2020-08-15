dd77fe4ad0238990473e2420726c1c341871d7c5
Yongun Seong <nevivurn@riseup.net>
2020-08-15 15:38:20 +0900

    Add helm

---
+++ roles/helm/files/env.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+DEVENV_HELM_BIN="${DEVENV_ROOT}/helm/current"
+
+if [[ -n "${DEVENV_DEBUG}" ]]; then
+	echo "[devenv/helm] helm version: $("${DEVENV_HELM_BIN}/helm" version)"
+fi
+
+if [[ -z "$DEVENV_HELM_SETUP" ]]; then
+	export PATH="${DEVENV_HELM_BIN}:${PATH}"
+fi
+export DEVENV_HELM_SETUP=1
