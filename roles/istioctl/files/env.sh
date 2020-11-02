60a24ff55cd667d69d0c7600a11e34f703b5fb27
Yongun Seong <nevil.seong@mathpresso.com>
2020-11-02 18:06:34 +0900

    Add istioctl

---
+++ roles/istioctl/files/env.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+DEVENV_ISTIOCTL_BIN="${DEVENV_ROOT}/istioctl/current"
+
+if [[ -n "${DEVENV_DEBUG}" ]]; then
+	echo "[devenv/istioctl] istioctl version: $("${DEVENV_ISTIOCTL_BIN}/bin/istioctl" --version)"
+fi
+
+if [[ -z "${DEVENV_ISTIOCTL_SETUP}" ]]; then
+	export PATH="${DEVENV_ROOT}/istioctl/istioctl/bin:${DEVENV_ISTIOCTL_BIN}/bin:${PATH}"
+fi
+export DEVENV_ISTIOCTL_SETUP=1
