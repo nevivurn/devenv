2746d36f65d8f8c0e2efb9c2fc0ff72af8e5be07
Yongun Seong <nevivurn@nevi.dev>
2022-01-15 22:44:00 +0900

    talosctl: add

---
+++ roles/talosctl/files/env.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+DEVENV_TALOSCTL_BIN="${DEVENV_ROOT}/talosctl/current"
+
+if [[ -n "${DEVENV_DEBUG}" ]]; then
+	echo "[devenv/talosctl] talosctl version: $("${DEVENV_TALOSCTL_BIN}/talosctl" version --client)"
+fi
+
+if [[ -z "$DEVENV_TALOSCTL_SETUP" ]]; then
+	export PATH="${DEVENV_TALOSCTL_BIN}:${PATH}"
+fi
+export DEVENV_TALOSCTL_SETUP=1
+
+#source <(talosctl completion bash)
