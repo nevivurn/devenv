eb050f476cbe877b10e6ad1f05c13bc21631e083
Yongun Seong <nevivurn@nevi.dev>
2022-08-21 17:08:04 +0900

    Remove unnecessary applications
    - kind
    - ngrok
    - packer
    - protoc
    - rust
    - talosctl

--- roles/rust/files/env.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-export CARGO_HOME="${DEVENV_ROOT}/rust/cargo"
-export RUSTUP_HOME="${DEVENV_ROOT}/rust/rustup"
-
-if [[ -z "${DEVENV_RUST_SETUP}" ]]; then
-	export PATH="${DEVENV_ROOT}/rust/init:${CARGO_HOME}/bin:${PATH}"
-fi
-export DEVENV_RUST_SETUP=1
